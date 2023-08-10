7c7d2a89a4720eb052ad1cdc701742c77d93aac7
Mumtaz Haqiqy <mumtaz.haqiqy@bythjul.com>
2023-08-11 00:19:24 +0200

    add filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -22,6 +24,11 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        return $this->hasVerifiedEmail();
+    }
 
     /**
      * The attributes that should be hidden for serialization.
